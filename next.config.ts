0eb7cb6f3e02da55dc439abe3180cde8f45a4304
seongho-lim15 <hws0602@naver.com>
2025-09-04 14:14:52 +0900

    api route 삭제제

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/lotte/:path*',
+        destination: 'https://company.lottemart.com/mobiledowa/:path*',
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
